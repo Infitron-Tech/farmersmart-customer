862e552b0cecc89887837b2b875689d96ef3cead
adetech2017 <adeyemi879@gmail.com>
2026-04-10 13:30:11 +0100

    deploy logo changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/farmers.ico" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
 
